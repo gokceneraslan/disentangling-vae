587bdd8d3a98a4c289ee7b1cef14b9ce1c5043b4
Gokcen Eraslan <gokcen.eraslan@gmail.com>
2019-08-11 12:38:46 -0400

    Add Cond models to the package root

--- disvae/__init__.py
+++ disvae/__init__.py
@@ -1,3 +1,3 @@
 from disvae.training import Trainer
 from disvae.evaluate import Evaluator
-from disvae.models.vae import VAEConv2D, VAEFC
+from disvae.models.vae import VAEConv2D, VAEFC CondVAEFC, CondVAEConv2D
